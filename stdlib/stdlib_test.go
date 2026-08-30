6e9946451fac86fb40885df2f8c595796222d3ee
Shashank Pachava <spachava753@gmail.com>
2026-08-30 16:04:57 -0400

    test(stdlib): discover vendored test modules

--- stdlib/stdlib_test.go
+++ stdlib/stdlib_test.go
@@ -1,25 +1,36 @@
 package stdlib_test
 
 import (
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/spachava753/bullsnake/internal/importer"
 	bullruntime "github.com/spachava753/bullsnake/internal/runtime"
 )
 
-func TestColorsys(t *testing.T) {
-	loader := importer.NewFileSystem("3.14/tests", "3.14")
-	spec, found, err := loader.Load(bullruntime.ModuleRequest{Name: "test_colorsys"})
+func TestVendoredStandardLibrary(t *testing.T) {
+	paths, err := filepath.Glob("3.14/tests/test_*.py")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !found {
-		t.Fatal("vendored test_colorsys module was not found")
-	}
-	if _, err := bullruntime.NewWithLoader(loader.Load).ExecuteModuleSpec(
-		"test_colorsys",
-		spec,
-	); err != nil {
-		t.Fatal(err)
+	loader := importer.NewFileSystem("3.14/tests", "3.14")
+	for _, path := range paths {
+		moduleName := strings.TrimSuffix(filepath.Base(path), ".py")
+		t.Run(moduleName, func(t *testing.T) {
+			spec, found, err := loader.Load(bullruntime.ModuleRequest{Name: moduleName})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !found {
+				t.Fatalf("vendored %s module was not found", moduleName)
+			}
+			if _, err := bullruntime.NewWithLoader(loader.Load).ExecuteModuleSpec(
+				moduleName,
+				spec,
+			); err != nil {
+				t.Fatal(err)
+			}
+		})
 	}
 }
